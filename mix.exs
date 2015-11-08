a02083270fa0afc17fc32ada9fd70e7d80117cf8
Alexey Barashkov <lnikell@gmail.com>
2015-11-08 21:33:59 +0500

    add exrm

--- mix.exs
+++ mix.exs
@@ -35,7 +35,9 @@ defmodule BootstrapGridPsd.Mixfile do
      {:postgrex, ">= 0.0.0"},
      {:phoenix_html, "~> 2.1"},
      {:phoenix_live_reload, "~> 1.0", only: :dev},
-     {:cowboy, "~> 1.0"}]
+     {:cowboy, "~> 1.0"},
+     {:exrm, "~> 0.19.9"}
+   ]
   end
 
   # Aliases are shortcut or tasks specific to the current project.
